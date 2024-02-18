b71e47bf83a21a64d455c0c16a32550713a18b67
elims <46279009+theelims@users.noreply.github.com>
2024-02-18 17:40:46 +0100

    New pattern RandomDepth

--- lib/StrokeEngine/pattern.h
+++ lib/StrokeEngine/pattern.h
@@ -138,7 +138,7 @@ protected:
 /**************************************************************************/
 /*!
   @brief  Simple pattern where the sensation value can change the speed
-  ratio between in and out. Sensation > 0 make the in move faster (up to 3x)
+  ratio between in and out. Sensation > 0 make the in move faster (up to 2x)
   giving a hard pounding sensation. Values < 0 make the out move going
   faster. This gives a more pleasing sensation. The time for the overall
   stroke remains the same.
@@ -193,7 +193,7 @@ protected:
     {
         // calculate the time it takes to complete the faster stroke
         // Division by 2 because reference is a half stroke
-        _timeOfFastStroke = (0.5 * _timeOfStroke) / fscale(0.0, 100.0, 1.0, 3.0, abs(_sensation), 0.0);
+        _timeOfFastStroke = (0.5 * _timeOfStroke) / fscale(0.0, 100.0, 1.0, 2.0, abs(_sensation), 0.0);
         // positive sensation, in is faster
         if (_sensation > 0.0)
         {
@@ -274,7 +274,7 @@ protected:
 /*!
   @brief  Like Teasing or Pounding, but every second stroke is only half the
   depth. The sensation value can change the speed ratio between in and out.
-  Sensation > 0 make the in move faster (up to 3x) giving a hard pounding
+  Sensation > 0 make the in move faster (up to 2x) giving a hard pounding
   sensation. Values < 0 make the out move going faster. This gives a more
   pleasing sensation. The time for the overall stroke remains the same for
   all strokes, even half ones.
@@ -346,7 +346,7 @@ protected:
     {
         // calculate the time it takes to complete the faster stroke
         // Division by 2 because reference is a half stroke
-        _timeOfFastStroke = (0.5 * _timeOfStroke) / fscale(0.0, 100.0, 1.0, 3.0, abs(_sensation), 0.0);
+        _timeOfFastStroke = (0.5 * _timeOfStroke) / fscale(0.0, 100.0, 1.0, 2.0, abs(_sensation), 0.0);
         // positive sensation, in is faster
         if (_sensation > 0.0)
         {
@@ -638,9 +638,85 @@ protected:
 
 /**************************************************************************/
 /*!
+  @brief  Like Teasing or Pounding, but every every stroke has a radom
+  depth between 10% and 100% of the stroke length. The sensation value
+  can change the speed ratio between in and out. Sensation > 0 make the in
+  move faster (up to 2x) giving a hard pounding sensation. Values < 0 make
+  the out move going faster. This gives a more pleasing sensation. The
+  speed for the stroke remains the same as for a full stroke.
+*/
+/**************************************************************************/
+class RandomDepth : public Pattern
+{
+public:
+    RandomDepth(const char *str) : Pattern(str) {}
+    void setSensation(float sensation)
+    {
+        _sensation = sensation;
+        _updateStrokeTiming();
+    }
+    void setTimeOfStroke(float speed = 0)
+    {
+        _timeOfStroke = speed;
+        _updateStrokeTiming();
+    }
+    motionParameters_t nextTarget(unsigned int index, bool retract = false)
+    {
+        // odd stroke is moving out
+        if (index % 2)
+        {
+            // maximum speed of the trapezoidal motion for the full stroke length
+            _nextMove.speed = 1.5 * _stroke / _timeOfOutStroke;
+
+            // acceleration to meet the profile for the shorter random stroke
+            _nextMove.acceleration = 3.0 * _nextMove.speed / (_timeOfOutStroke * _randomFactor);
+            _nextMove.stroke = 0;
+        }
+        // even stroke is moving in
+        else
+        {
+            // get new random value
+            _randomFactor = random(10, 100) / 100.0;
+
+            // maximum speed of the trapezoidal motion for the full stroke length
+            _nextMove.speed = 1.5 * _stroke / _timeOfInStroke;
+
+            // acceleration to meet the profile for the shorter random stroke
+            _nextMove.acceleration = 3.0 * _nextMove.speed / (_timeOfInStroke * _randomFactor);
+            _nextMove.stroke = _stroke * _randomFactor;
+        }
+        _index = index;
+        return _nextMove;
+    }
+
+protected:
+    float _timeOfFastStroke = 1.0;
+    float _timeOfInStroke = 1.0;
+    float _timeOfOutStroke = 1.0;
+    float _randomFactor = 1.0;
+    void _updateStrokeTiming()
+    {
+        // calculate the time it takes to complete the faster stroke
+        // Division by 2 because reference is a half stroke
+        _timeOfFastStroke = (0.5 * _timeOfStroke) / fscale(0.0, 100.0, 1.0, 2.0, abs(_sensation), 0.0);
+        // positive sensation, in is faster
+        if (_sensation > 0.0)
+        {
+            _timeOfInStroke = _timeOfFastStroke;
+            _timeOfOutStroke = _timeOfStroke - _timeOfFastStroke;
+        }
+        // negative sensation, out is faster
+        else
+        {
+            _timeOfOutStroke = _timeOfFastStroke;
+            _timeOfInStroke = _timeOfStroke - _timeOfFastStroke;
+        }
+    }
+};
+
+/**************************************************************************/
+/*!
   TODO: New Pattern Ideas
-    - RandomDepth: Like HalfnHalf, but the depth is random between 0 and 100%.
-                Stroke Velocity is constant. Shorter strokes are faster paced.
     - KnottyJoy: A series of rapid 50% depth strokes followed by a regular
                 speed 100% stroke and a pause. Sensation controls the number
                 of rapid strokes and the hold time.
@@ -658,7 +734,8 @@ static Pattern *patternTable[] = {
     new HalfnHalf("Half'n'Half"),
     new Deeper("Deeper"),
     new StopNGo("Stop'n'Go"),
-    new Insist("Insist")
+    new Insist("Insist"),
+    new RandomDepth("RandomDepth")
     // <-- insert your new pattern class here!
 };
 
