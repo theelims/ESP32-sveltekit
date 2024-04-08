6f0a1f18162f7d7d9cd1ed87de943dc1d5374ee7
Rune Harlyk <rune@harlyk.dk>
2024-04-08 21:54:52 +0200

    Pass fetch in load function

--- interface/src/routes/+layout.ts
+++ interface/src/routes/+layout.ts
@@ -4,7 +4,7 @@ import type { LayoutLoad } from './$types';
 export const prerender = false;
 export const ssr = false;
 
-export const load = (async () => {
+export const load = (async ({ fetch }) => {
 	const result = await fetch('/rest/features');
 	const item = await result.json();
 	return {
