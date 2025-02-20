e427c9c4c04cdcbeaaf352f1c6a2cf7305ac629d
elims <elims@gmx.net>
2025-02-20 01:20:50 +0100

    Upgrade Sveltekit 2 and vite 5

--- interface/vite.config.ts
+++ interface/vite.config.ts
@@ -16,12 +16,12 @@ const config: UserConfig = {
 		proxy: {
 			// Proxying REST: http://localhost:5173/rest/bar -> http://192.168.1.83/rest/bar
 			'/rest': {
-				target: 'http://192.168.1.89',
+				target: 'http://192.168.1.107',
 				changeOrigin: true
 			},
 			// Proxying websockets ws://localhost:5173/ws -> ws://192.168.1.83/ws
 			'/ws': {
-				target: 'ws://192.168.1.89',
+				target: 'ws://192.168.1.107',
 				changeOrigin: true,
 				ws: true
 			}
