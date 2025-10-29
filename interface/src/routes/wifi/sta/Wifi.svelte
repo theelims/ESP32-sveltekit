669ed21cdb4dd5534bb91f61eacdf3c6b8f36949
elims <elims@gmx.net>
2025-10-29 01:15:54 +0100

    WiFi Station Responsive

--- interface/src/routes/wifi/sta/Wifi.svelte
+++ interface/src/routes/wifi/sta/Wifi.svelte
@@ -32,7 +32,7 @@
 	import Add from '~icons/tabler/circle-plus';
 	import Edit from '~icons/tabler/pencil';
 	import Delete from '~icons/tabler/trash';
-	import Grip from '~icons/tabler/grip-horizontal';
+	import Grip from '~icons/tabler/grip-vertical';
 	import Cancel from '~icons/tabler/x';
 	import Check from '~icons/tabler/check';
 	import Save from '~icons/tabler/device-floppy';
@@ -256,6 +256,13 @@
 	async function getWifiData() {
 		await getWifiStatus();
 		await getWifiSettings();
+	}
+
+	function truncateSSID(ssid: string, maxLength: number = 15): string {
+		if (ssid.length <= maxLength) {
+			return ssid;
+		}
+		return ssid.substring(0, maxLength) + '...';
 	}
 </script>
 
@@ -498,21 +505,30 @@
 						>
 							{#snippet children({ item: network, index }: { item: any; index: number })}
 								<!-- svelte-ignore a11y_click_events_have_key_events -->
-								<div class="rounded-box bg-base-100 flex items-center space-x-3 px-4 py-2">
-									<Grip class="h-6 w-6 text-base-content/30 cursor-grab" />
-									<div class="mask mask-hexagon bg-primary h-auto w-10 shrink-0">
+								<div class="rounded-box bg-base-100 flex items-center gap-2 px-2 py-2">
+									<Grip class="h-6 w-6 text-base-content/30 cursor-grab flex-shrink-0" />
+									<div class="mask mask-hexagon bg-primary h-auto w-10 flex-shrink-0">
 										<Router class="text-primary-content h-auto w-full scale-75" />
 									</div>
-									<div>
-										<div class="font-bold">{network.ssid}</div>
+									<div class="flex flex-1 gap-x-2 min-w-0">
+										<div class="font-bold truncate min-w-0" title={network.ssid}>
+											{network.ssid}
+										</div>
+										{#if network.static_ip_config}
+											<div
+												class="badge badge-sm badge-secondary opacity-75 shrink-0 hidden sm:block"
+											>
+												Static
+											</div>
+										{:else}
+											<div
+												class="badge badge-sm badge-outline badge-secondary opacity-75 shrink-0 hidden sm:block"
+											>
+												DHCP
+											</div>
+										{/if}
 									</div>
-									{#if network.static_ip_config}
-										<div class="badge badge-sm badge-secondary opacity-75">Static</div>
-									{:else}
-										<div class="badge badge-sm badge-outline badge-secondary opacity-75">DHCP</div>
-									{/if}
-									<div class="grow"></div>
-									<div class="space-x-0 px-0 mx-0">
+									<div class="flex items-center gap-x-1 flex-shrink-0">
 										<button
 											class="btn btn-ghost btn-sm"
 											onclick={() => {
